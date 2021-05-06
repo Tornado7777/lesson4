70337fa618435cd0acc80997f5888a14b2443d32
Ivan <tornadod@mail.ru>
2021-05-07 01:06:04 +0300

    quset 4.1 ready

--- lesson4.1/Program.cs
+++ lesson4.1/Program.cs
@@ -6,7 +6,33 @@ namespace lesson4._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] PLF = new string[4];
+            int i = 0;
+            for ( i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Для окончания ввода ФИО наберите exit.");
+                Console.WriteLine($"Вы еще можете ввести {4-i} ФИО");
+                Console.WriteLine("Введите Фамилию:");
+                string firstName = Console.ReadLine();
+                if (firstName == "exit") break;
+                Console.WriteLine("Введите Имя:");
+                string lastName = Console.ReadLine();
+                if (lastName == "exit") break;
+                Console.WriteLine("Введите Отчество:");
+                string patronymic = Console.ReadLine();
+                if (patronymic == "exit") break;
+                PLF[i] = GetFullName(firstName, lastName, patronymic);
+                Console.Clear();
+            }
+            int j = i;
+            for (i = 0; i < j; i++)
+            {
+                Console.WriteLine(PLF[i]);
+            }
+        }
+        static string GetFullName(string firstName, string lastName, string patronymic) //метод(ф-ция) соединения ФИО в строку
+        {
+            return firstName + " " + lastName + " " + patronymic;
         }
     }
 }
