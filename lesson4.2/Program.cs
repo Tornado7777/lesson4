1f505aa9e5c26e11f08c7f6a16bc542780591f20
Ivan <tornadod@mail.ru>
2021-05-07 02:40:41 +0300

    ready quest4.2

--- lesson4.2/Program.cs
+++ lesson4.2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*2.	Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, 
+ * и возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести 
+ * результат на экран.
+ */
+using System;
 
 namespace lesson4._2
 {
@@ -6,7 +10,22 @@ namespace lesson4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите набор целых чисел разделяя числа пробелом, для получения их суммы:");
+            string NumberText = Console.ReadLine();
+            string CurrentNum = "0";
+            int sum = 0;
+            for (int i=0;i<NumberText.Length;i++)
+            {
+                while (i < NumberText.Length)
+                {
+                    if (NumberText[i].ToString() == " ")  break;
+                    CurrentNum = CurrentNum + NumberText[i];
+                    i++;
+                }
+                sum = sum + Convert.ToInt32(CurrentNum);
+                CurrentNum = "0";
+            }
+            Console.WriteLine(sum);
         }
     }
 }
